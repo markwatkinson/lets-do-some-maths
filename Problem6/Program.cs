7adee0e211a594b1f58adf5f9cc95727cdfd2434
Mark Watkinson <markwatkinson@gmail.com>
2012-07-26 17:11:57 +0100

    Problem 6

---
+++ Problem6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<long> range = Euler.Utils.Range(1, 101);
+            IEnumerable<long> squares = range.Select(a => a * a);
+
+            long sumOfSquares = squares.Sum();
+            long sum = range.Sum();
+            long squareOfSum = sum * sum;
+            Euler.Utils.OutputAnwser(Math.Abs(sumOfSquares - squareOfSum).ToString());
+
+
+        }
+    }
+}
