bf044fce71b3ed905c19d0b919c8dd28bc0e279e
Mark Watkinson <markwatkinson@gmail.com>
2012-07-27 15:00:14 +0100

    Problem 16

---
+++ Problem016/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+namespace Problem016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger b = BigInteger.Pow(2, 1000);
+            
+            IEnumerable<int> digits = b.ToString().Select(c => Convert.ToInt32(c.ToString()));
+            long sum = digits.Sum();
+            Euler.Utils.OutputAnswer(sum.ToString());
+            
+        }
+    }
+}
