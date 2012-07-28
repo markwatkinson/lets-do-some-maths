ed4c0d57f28d1f6b3d192a9af22eb0cd339e3982
Mark Watkinson <markwatkinson@gmail.com>
2012-07-28 11:53:56 +0100

    Problem 20

---
+++ Problem020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger b = new BigInteger(100);
+            for (int i = 99; i >= 2; i--)
+            {
+                b *= i;
+            }
+
+            IEnumerable<int> digits = b.ToString().Select(x => Convert.ToInt32(x.ToString()));
+            long sum = digits.Sum();
+
+            Euler.Utils.OutputAnswer(sum.ToString());
+        }
+    }
+}
