96b6326710ad36ddfd3908d82a87baf0e5c90904
Mark Watkinson <markwatkinson@gmail.com>
2012-08-01 14:34:40 +0100

    problem 29

---
+++ Problem029/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger pow;
+            var results = new List<BigInteger>();
+
+            for (int a = 2; a <= 100; a++)
+            {
+                for (int b = 2; b <= 100; b++)
+                {
+                    results.Add(
+                        BigInteger.Pow(new BigInteger(a), b)
+                    );
+                }
+            }
+            Euler.Utils.OutputAnswer(results.Distinct().Count().ToString());
+        }
+    }
+}
