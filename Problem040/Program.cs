3ca5a1c6ee01d352bd7086e36993bab8c55a34f1
Mark Watkinson <markwatkinson@gmail.com>
2012-08-05 15:27:42 +0100

    Problem 40

---
+++ Problem040/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is pretty slow but still within 1 minute
+            string s = "";
+            long i = 1;
+            while (s.Length < 1000000) {
+                s += i.ToString();
+                i++;
+            }
+            int product = 1;
+            for (i = 0; i <= 6; i++)
+            {
+                int index = System.Convert.ToInt32(Math.Pow(10, i)) - 1;
+                product *= System.Convert.ToInt32("" + s[index]);
+            }
+            Euler.Utils.OutputAnswer(product.ToString());
+
+        }
+    }
+}
